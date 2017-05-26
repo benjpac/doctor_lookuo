40df22fc794243896a851eb015f16a12df77ce6d
Ben Metzger <benjpac@gmail.com>
2017-05-26 14:09:05 -0700

    start integrating maps api to make this assignment more interesting

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,16 +1,12 @@
 var getDoctors = require('./../js/doctor.js').getDoctors;
 
-var displayDoctor = function(response){
-  debugger;
-  response.data.forEach(function(doctor){
-    console.log(doctor);
-    var image_url = doctor.profile.image_url;
+var displayDoctor = function(result){
+  result.data.forEach(function(doctor){
     var name = doctor.profile.first_name + ' ' + doctor.profile.last_name + ' ' + doctor.profile.title;
-    console.log(name);
+    var imageURL = doctor.profile.image_url;
     var specialty = doctor.specialties[0].actor;
-    console.log(specialty);
     var description = doctor.specialties[0].description;
-    console.log(description);
+    var newDoctor = new Doctor()
   })
 }
 
--- js/doctor.js
+++ js/doctor.js
@@ -10,3 +10,29 @@ exports.getDoctors = function(symptoms, displayDoctor) {
       console.log("fail");
     });
 };
+
+function Doctor(name, city, imageURL, about) {
+  current_person = this;
+  current_person.name = name;
+  current_person.city = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + city + '&key=' + apiKey;
+  current_person.imageURL = imageURL;
+  current_person.about = about;
+  current_person.location = '';
+  current_person.createMarker = function(map) {
+    var marker = new google.maps.Marker({
+      position: current_person.location,
+      map: map
+    });
+    marker.addListener('click', function() {
+      map.setZoom(8);
+      map.setCenter(marker.getPosition());
+      $('#profile').show();
+      $('#nameID').text(current_person.name);
+      $('#imageID').attr('src', current_person.imageURL);
+      $('#ageID').text(current_person.age);
+      $('#aboutID').text(current_person.about);
+    });
+  };
+}
+
+exports.personModule = Person;
