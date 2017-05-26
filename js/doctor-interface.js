41ac080c35c215cd88af72b00ea2f1a4f0168e86
Ben Metzger <benjpac@gmail.com>
2017-05-26 13:48:39 -0700

    add working function to return doctor data based on symptoms

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,10 +1,23 @@
-var Doctor = require('./../js/doctor.js').doctorModule;
+var getDoctors = require('./../js/doctor.js').getDoctors;
 
-$(function() {
- $("form#findDoctor").submit(function(event) {
+var displayDoctor = function(response){
+  debugger;
+  response.data.forEach(function(doctor){
+    console.log(doctor);
+    var image_url = doctor.profile.image_url;
+    var name = doctor.profile.first_name + ' ' + doctor.profile.last_name + ' ' + doctor.profile.title;
+    console.log(name);
+    var specialty = doctor.specialties[0].actor;
+    console.log(specialty);
+    var description = doctor.specialties[0].description;
+    console.log(description);
+  })
+}
+
+$(document).ready(function() {
+ $("form#find-doctor").submit(function(event) {
   event.preventDefault();
   var symptoms = $('#symptoms').val();
-  var doctor = getDoctors(symptoms);
-  console.log(doctor)
+  getDoctors(symptoms, displayDoctor);
   })
 })
--- js/doctor.js
+++ js/doctor.js
@@ -1,14 +1,12 @@
 var apiKey = require('./../.env').apiKey;
 
 
-exports.getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+exports.getDoctors = function(symptoms, displayDoctor) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ symptoms+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
-      console.log(result);
+      displayDoctor(result);
     })
    .fail(function(error){
       console.log("fail");
     });
 };
-
-exports.doctorModule = Doctor;
